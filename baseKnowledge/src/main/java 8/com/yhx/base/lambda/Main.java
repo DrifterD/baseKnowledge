8279a948b67e78efa8f23c0cd00d60b2915377a8
moon <yangdrifter@163.com>
2018-01-11 20:39:24 +0800

    add java 8 new features

--- baseKnowledge/src/main/java 8/com/yhx/base/lambda/Main.java
+++ baseKnowledge/src/main/java 8/com/yhx/base/lambda/Main.java
@@ -3,33 +3,37 @@ package com.yhx.base.lambda;
 import com.yhx.base.functionalInterface.FunctionalInterfaceMain;
 import com.yhx.base.functionalInterface.ParamInterface;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 /**
- *
  * lambda 表达式语法：（paramType paramName） ->{ ......}.
- *
+ * <p>
  * 1.lambda 表达式可以实现任何接只有一个需要实现方法的接口。多个实现方法使用lambda时，编译异常
  * 2.lambda 目标对象（实现类）必须是接口
+ * 3. jdk 提供幾個內部接口 以便常規開發使用：
+ *
  *
  */
 public abstract class Main {
 
-     abstract void t();
+    abstract void t();
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         //example2Runnable();
         //exampleInterface();
-        paramInterface();
-
+        //paramInterface();
+        functionMethod();
         System.out.println("done");
 
 
     }
 
     //这里以runnable 接口为例
-    static void example2Runnable()throws Exception{
+    static void example2Runnable() throws Exception {
 
         //1. 匿名内部类
-        Thread t=new Thread(new Runnable() {
+        Thread t = new Thread(new Runnable() {
 
             @Override
             public void run() {
@@ -38,7 +42,7 @@ public abstract class Main {
             }
         });
 
-        Thread tLambda=new Thread(()->{
+        Thread tLambda = new Thread(() -> {
             //do something
             System.out.println("lambda表达式调用");
         });
@@ -50,16 +54,16 @@ public abstract class Main {
     }
 
     //这里以functionInterface 接口为例
-    static void exampleInterface(){
+    static void exampleInterface() {
 
-        FunctionalInterfaceMain fim=()->{
+        FunctionalInterfaceMain fim = () -> {
             System.out.println("通过 lamada表达式实现接口抽象方法");
         };
 
         fim.test();
 
         //内部匿名类
-        FunctionalInterfaceMain fim2=new FunctionalInterfaceMain() {
+        FunctionalInterfaceMain fim2 = new FunctionalInterfaceMain() {
             @Override
             public void test() {
                 System.out.println("通过内部匿名类表达式实现接口抽象方法");
@@ -68,12 +72,12 @@ public abstract class Main {
     }
 
 
-    static void paramInterface(){
+    static void paramInterface() {
 
-        ParamInterface pi=new ParamInterface() {
+        ParamInterface pi = new ParamInterface() {
             @Override
             public String hello(String name) {
-                return "hello "+name;
+                return "hello " + name;
             }
 //
 //            @Override
@@ -82,9 +86,29 @@ public abstract class Main {
 //            }
         };
 
-        ParamInterface pi2=(String name)->{ return "hello lamabd "+name; };
+        ParamInterface pi2 = (String name) -> {
+            return "hello lamabd " + name;
+        };
 
-        System.out.println("pi result:"+pi.hello("yhx"));
-        System.out.println("pi2 result:"+pi2.hello("yhx"));
+        System.out.println("pi result:" + pi.hello("yhx"));
+        System.out.println("pi2 result:" + pi2.hello("yhx"));
+    }
+
+    //jdk 提供 function<paramType,returnType> 接口
+    static void functionMethod() {
+
+        Function<String, Integer> fc = (String num) -> {
+            System.out.println("lambda string" + num + " convert int");
+            return Integer.valueOf(num);
+        };
+
+        Predicate<Integer> predicate=(Integer i)->{
+
+            System.out.println("lambda Predicate i=" + i + "  Predicate");
+            return i>0;
+        };
+
+        System.out.println("predicate "+predicate.test(10));
+        System.out.println(fc.apply("1234"));
     }
 }
